36794eef3fe7afa2bc331b025f7b5d3a15640b5b
abaldeg <abaldeg@gmail.com>
2020-06-02 11:22:48 -0300

    Generacion de archivos con Multiples Hilos

--- Genera Archivo Grande.py
+++ Genera Archivo Grande.py
@@ -1,5 +1,9 @@
 import random
 import string
+import time
+from threading import Thread
+
+tamaño=2048
 
 def genera_archivo_aleatorio(nombre,tamaño):
     """generate big random letters/alphabets to a file
@@ -9,11 +13,27 @@ def genera_archivo_aleatorio(nombre,tamaño):
     """
     
     try:
-        chars = ''.join([random.choice(string.ascii_letters) for i in range(tamaño)]) #1
+        chars = ''.join([random.choice(string.ascii_letters) for i in range(tamaño*tamaño)]) #1
         with open(nombre, 'w') as a:
                     a.write(chars)
     finally:
         a.close()
     
 
-genera_archivo_aleatorio("C:\\Users\\abald\\OneDrive\\Documentos\\GitHub\\TP_ARQ_Threads\\archivos.txt",10)
+# Crea thread 1 y lo referencia con t1
+t1 = Thread(target=genera_archivo_aleatorio, args=("C:\\Users\\abald\\OneDrive\\Documentos\\GitHub\\TP_ARQ_Threads\\archivos\\archivos.txt",tamaño)
+# Crea thread 2 y lo referencia con t2
+t2 = Thread(target=genera_archivo_aleatorio, args=("C:\\Users\\abald\\OneDrive\\Documentos\\GitHub\\TP_ARQ_Threads\\archivos\\archivos.txt",tamaño)
+
+# Marca de tiempo
+start = time.time()
+t1.start()  # inicia thread1
+t2.start()  # inicia thread2
+t1.join()   # espera a que thread1 retorne/termine
+t2.join()   # espera thread2
+# Marca de tiempo final
+end = time.time()
+
+# tiempo consumido
+print('Tiempo consumido en segundos -', end - start)
+genera_archivo_aleatorio("C:\\Users\\abald\\OneDrive\\Documentos\\GitHub\\TP_ARQ_Threads\\archivos\\archivos.txt",tamaño)
